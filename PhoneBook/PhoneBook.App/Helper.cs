eae97952d0afae83d061985a66e417310b9b74e9
Milos <kenguruubre@gmail.com>
2022-08-27 22:48:16 +0200

    add method added to calculator class

---
+++ PhoneBook/PhoneBook.App/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.App
+{
+    internal static class Helper
+    {
+
+        public static int Add(int x, int y)
+        {
+            return 0;
+        }
+    }
+}
--- PhoneBook/PhoneBook.App/Program.cs
+++ PhoneBook/PhoneBook.App/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Cmok");
 Console.WriteLine("Tijana");
+int sum = PhoneBook.App.Helper.Add(4, 5);
+Console.WriteLine(sum);
 Console.ReadKey();  
