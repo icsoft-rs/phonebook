06e448e49359ba66af7e8c3c3a8dac69a5154726
Tijana <tijana88iv@gmail.com>
2022-08-28 14:13:56 +0200

    Finished

--- PhoneBook/PhoneBook.App/Program.cs
+++ PhoneBook/PhoneBook.App/Program.cs
@@ -3,4 +3,6 @@ Console.WriteLine("Cmok");
 Console.WriteLine("Tijana");
 int sum = PhoneBook.App.Helper.Add(4, 5);
 Console.WriteLine(sum);
+decimal multi = PhoneBook.App.Helper.Multiplication(6, 7);
+Console.WriteLine(multi);
 Console.ReadKey();  
